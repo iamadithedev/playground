2061d300ae3c82dd1e77dd1f5c2aa45a812f99c4
iamadithedev <iusca.adrian@gmail.com>
2023-04-04 21:59:46 +0300

    make use of Transform class

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,3 +1,9 @@
+#include "vec3.hpp"
+#include "transform.hpp"
+
+#include <vector>
+#include <iostream>
+
 #include <imgui.h>
 
 #include <backends/imgui_impl_glfw.h>
@@ -6,36 +12,36 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
+#include <assimp/Importer.hpp>
+#include <assimp/postprocess.h>
+#include <assimp/scene.h>
 
 const char* vertex_shader_text =
-"#version 330\n"
+"#version 420\n"
 "in vec3 in_position;\n"
-"uniform mat4 u_mvp;\n"
+"layout (binding = 0, std140) uniform matrices\n"
+"{\n"
+"   mat4 model;\n"
+"   mat4 view;\n"
+"   mat4 proj;\n"
+"};\n"
 "void main()\n"
 "{\n"
-"   gl_Position = u_mvp * vec4(in_position, 1.0);\n"
+"   gl_Position = proj * view * model * vec4(in_position, 1.0);\n"
 "}\n";
 
 const char* fragment_shader_text =
-"#version 330\n"
+"#version 430\n"
+"layout (location = 0) uniform vec3 u_color;\n"
 "out vec4 out_color;\n"
 "void main()\n"
 "{\n"
-"   out_color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+"   out_color = vec4(u_color, 1.0);\n"
 "}\n";
 
 struct vertex
 {
-    float x, y, z;
-};
-
-vertex vertices[3] =
-{
-{ -0.6f, -0.4f, 0.0f },
-{ 0.6f, -0.4f, 0.0f },
-{ 0.0f, 0.6f, 0.0f }
+    vec3 position;
 };
 
 void key_callback(GLFWwindow* window, int key, int, int action, int)
@@ -46,6 +52,36 @@ void key_callback(GLFWwindow* window, int key, int, int action, int)
         {
             glfwSetWindowShouldClose(window, GLFW_TRUE);
         }
+    }
+}
+
+void shader_status(uint32_t handle)
+{
+    int  success;
+    char info[512];
+
+    glGetShaderiv(handle, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        glGetShaderInfoLog(handle, 512, nullptr, info);
+
+        std::cerr << "shader compilation failed\n" << info << std::endl;
+    }
+}
+
+void program_status(uint32_t handle)
+{
+    int  success;
+    char info[512];
+
+    glGetProgramiv(handle, GL_LINK_STATUS, &success);
+
+    if (!success)
+    {
+        glGetProgramInfoLog(handle, 512, nullptr, info);
+
+        std::cerr << "program linking failed\n" << info << std::endl;
     }
 }
 
@@ -85,20 +121,61 @@ int main()
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 130");
 
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    // ==================================================================================
 
     uint32_t vertex_shader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
     glCompileShader(vertex_shader);
+    shader_status(vertex_shader);
 
     uint32_t fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
     glCompileShader(fragment_shader);
+    shader_status(fragment_shader);
 
     uint32_t program = glCreateProgram();
     glAttachShader(program, vertex_shader);
     glAttachShader(program, fragment_shader);
     glLinkProgram(program);
+    program_status(program);
+
+    // ==================================================================================
+
+    Assimp::Importer importer;
+
+    std::vector<vertex> x_vertices;
+    std::vector<uint32_t> x_indices;
+
+    const aiScene* scene = importer.ReadFile("../x.obj", 0);
+
+    if (scene && scene->mRootNode)
+    {
+        const aiNode* node = scene->mRootNode->mChildren[0];
+
+        for (uint32_t i = 0; i < node->mNumMeshes; i++)
+        {
+            const aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
+
+            for (uint32_t j = 0; j < mesh->mNumVertices; j++)
+            {
+                const aiVector3D& position = mesh->mVertices[j];
+
+                x_vertices.push_back({ position.x, position.y, position.z });
+            }
+
+            for (uint32_t j = 0; j < mesh->mNumFaces; j++)
+            {
+                const aiFace& face = mesh->mFaces[j];
+
+                for (uint32_t f = 0; f < face.mNumIndices; f++)
+                {
+                    x_indices.push_back(face.mIndices[f]);
+                }
+            }
+        }
+    }
+
+    // ==================================================================================
 
     uint32_t vertex_array;
     glGenVertexArrays(1, &vertex_array);
@@ -107,12 +184,32 @@ int main()
     uint32_t vertex_buffer;
     glGenBuffers(1, &vertex_buffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex) * x_vertices.size(), x_vertices.data(), GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vertex), (void*) 0);
+    uint32_t indices_buffer;
+    glGenBuffers(1, &indices_buffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices_buffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint32_t) * x_indices.size(), x_indices.data(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vertex), (void*) offsetof(vertex, position));
     glEnableVertexAttribArray(0);
 
+    // ==================================================================================
+
+    uint32_t matrices_buffer;
+    glGenBuffers(1, &matrices_buffer);
+    glBindBufferBase(GL_UNIFORM_BUFFER, 0, matrices_buffer);
+
+    // ==================================================================================
+
     glEnable(GL_MULTISAMPLE);
+
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    std::vector<glm::mat4> matrices { 3 };
+    glm::vec3 triangle_color { 1.0f, 0.0f, 0.0f };
+
+    Transform x_transform;
 
     while (!glfwWindowShouldClose(window))
     {
@@ -128,9 +225,9 @@ int main()
 
         ImGui::NewFrame();
 
-        ImGui::Begin("Test");
-        ImGui::Text("Text");
+        ImGui::Begin("RenderPass");
         ImGui::ColorEdit3("Clear color", (float*) &clear_color, ImGuiColorEditFlags_NoOptions);
+        ImGui::ColorEdit3("Triangle color", (float*) &triangle_color, ImGuiColorEditFlags_NoOptions);
         ImGui::End();
 
         ImGui::Render();
@@ -138,19 +235,25 @@ int main()
         glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glm::mat4 model = glm::translate(glm::mat4(1.0f), { 0.0f, 0.0f, 0.0f });
-                  model = glm::rotate(model, (float) glfwGetTime(), { 0.0f, 0.0f, 1.0f });
+        x_transform.translate({ 0.0f, 0.0f, 0.0f })
+                   .rotate({ 0.0f, 0.0f, 1.0f }, (float) glfwGetTime())
+                   .scale({ 0.5f, 0.5f, 0.5f });
 
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), { 0.0f, 0.0f, -3.0f });
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), { 0.0f, 0.0f, -5.0f });
         glm::mat4 proj = glm::perspective(glm::radians(60.0f), ratio, 0.1f, 100.0f);
 
-        glm::mat4 mvp = proj * view * model;
+        matrices[0] = x_transform.matrix();
+        matrices[1] = view;
+        matrices[2] = proj;
+
+        glBufferData(GL_UNIFORM_BUFFER, sizeof(glm::mat4) * matrices.size(), matrices.data(), GL_STATIC_DRAW);
 
         glUseProgram(program);
-        glUniformMatrix4fv(0, 1, GL_FALSE, (const float*) &mvp);
+        glUniform3fv(0, 1, (const float*) &triangle_color);
 
         glBindVertexArray(vertex_array);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, (int32_t) x_indices.size(), GL_UNSIGNED_INT, 0);
 
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
